c70e06237b5eca3fa4e6b2ec76af84ba6e113f9f
unknown <santierick.38@gmail.com>
2023-05-31 19:00:42 -0500

    baseExamen

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/Card'
-
-const cardItems = [
-  {
-    name: "Primer carddd",
-    color: "red",
-  },
-  {
-    name: "Second cardddd",
-    color: "blue",
-  }
-]
-
-const defaultCustomCard = {
-  name: "Card manejada por Hook",
-  color: "yellow"
-}
+import Card from './components/card/Card'
 
 function App() {
-  const [customCard, setCustomCard] = useState(defaultCustomCard)
 
-  const cambiarNombre = (e) => {
-      setCustomCard(prevState => ({...prevState, name: e.target.value}))
-  }
-    
-  const cambiarColor = (e) => {
-      setCustomCard(prevState => ({...prevState, color: e.target.value}))
-  }
 
   return (
-    <>
-      <p>Cambiando el nombre:</p>
-      <input onChange={cambiarNombre} defaultValue={defaultCustomCard.name} />
-
-      <p>Cambiando el color:</p>
-      <input onChange={cambiarColor} defaultValue={defaultCustomCard.color}/>
-
-      {cardItems.map((item) => 
-        <Card key={item.name} item={item}>
-          <h1>Este es el titulo insertado por children</h1>
-        </Card>
-      )}
-      <Card item={customCard}>
-      <h1>Este es el titulo insertado por children</h1>
-      </Card>
-    </>
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card soyLaProp='Hola, soy una prop' />
+    </div>
   )
 }
 
